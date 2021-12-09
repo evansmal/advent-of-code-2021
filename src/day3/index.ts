686e6dc86462dfe9724cde98b065ef808165c85f
Evan Smal <evan.smal@hotmail.com>
2021-12-09 07:11:41 -0500

    Completed day 3 part 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,33 +1,73 @@
 import { loadInputDataFromDay } from "../common/input.js";
 
-function toBinarySequence(str: string) {
+type BinarySequence = (0 | 1)[];
+
+function toBinarySequence(str: string): BinarySequence {
     return str.split("").map(character => (character === '1' ? 1 : 0));
 }
 
-function countHighBit(input: string[]): number[] {
-    const counts = input.map(toBinarySequence);
-    const sums = counts.reduce((prev, curr) => {
-        for (let i = 0; i < curr.length; i++) {
-            curr[i] += prev[i];
-        }
-        return curr;
-    }, Array(counts[0].length).fill(0));
+function countHighBitAtPosition(input: BinarySequence[], pos: number) {
+    return input.filter(seq => (seq[pos] === 1)).length;
+}
+
+function countHighBit(input: BinarySequence[]): number[] {
+    const width = input[0].length;
+    const sums: number[] = [];
+    for (let i = 0; i < width; i++) {
+        sums.push(countHighBitAtPosition(input, i));
+    }
     return sums;
 }
 
-function main() {
-    const inputs = loadInputDataFromDay(3);
-    const gamma_counts = countHighBit(inputs).map(count => {
+function part1(inputs: string[]) {
+    const gamma_counts = countHighBit(inputs.map(toBinarySequence)).map(count => {
         return (count > inputs.length / 2 ? 1 : 0);
     });
-    const epsilon_counts = countHighBit(inputs).map(count => {
+    const epsilon_counts = countHighBit(inputs.map(toBinarySequence)).map(count => {
         return (count <= inputs.length / 2 ? 1 : 0);
     });
 
     const gamma_rate = parseInt(gamma_counts.join(""), 2);
     const epsilon_rate = parseInt(epsilon_counts.join(""), 2);
 
+    return { gamma_rate, epsilon_rate };
+}
+
+function part2(inputs: string[]) {
+    const sequences = inputs.map(toBinarySequence);
+    const width = sequences[0].length;
+
+    let oxygen_gen_rating: BinarySequence[] = sequences;
+    for (let i = 0; i < width; i++) {
+        if (oxygen_gen_rating.length === 1) break;
+        const count = countHighBitAtPosition(oxygen_gen_rating, i);
+        if (count >= oxygen_gen_rating.length / 2) oxygen_gen_rating = oxygen_gen_rating.filter(seq => seq[i] === 1);
+        else oxygen_gen_rating = oxygen_gen_rating.filter(seq => seq[i] === 0);
+    }
+
+    let co2_scrubber_rating: BinarySequence[] = sequences;
+    for (let i = 0; i < width; i++) {
+        if (co2_scrubber_rating.length === 1) break;
+        const count = countHighBitAtPosition(co2_scrubber_rating, i);
+        if (count >= co2_scrubber_rating.length / 2) co2_scrubber_rating = co2_scrubber_rating.filter(seq => seq[i] === 0);
+        else co2_scrubber_rating = co2_scrubber_rating.filter(seq => seq[i] === 1);
+    }
+
+    return {
+        oxygen_gen_rating: parseInt(oxygen_gen_rating[0].join(""), 2),
+        co2_scrubber_rating: parseInt(co2_scrubber_rating[0].join(""), 2)
+    };
+}
+
+function main() {
+    const inputs = loadInputDataFromDay(3);
+
+    const { gamma_rate, epsilon_rate } = part1(inputs);
     console.log(gamma_rate * epsilon_rate);
+
+    const { oxygen_gen_rating, co2_scrubber_rating } = part2(inputs);
+    console.log(oxygen_gen_rating * co2_scrubber_rating)
+
 }
 
 main();
