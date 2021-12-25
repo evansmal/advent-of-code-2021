48b888e0259a4c479ae95169ec7c99007e3e1e49
Evan Smal <evan.smal@hotmail.com>
2021-12-24 21:13:17 -0500

    Refactored day 7 p1

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,17 +1,32 @@
 import { loadInputDataFromDay } from "../common/input.js";
 
-function part1(inputs: number[]) {
+function findLowestCost(inputs: number[], get_cost: (pos: number, target: number) => number) {
     const max = Math.max(...inputs);
+
     const positions: number[] = Array(max + 1).fill(0);
     inputs.forEach(value => { positions[value]++; });
 
     const costs = Array(max + 1).fill(0).map((_, pos) => {
         return positions.map((count, curr_pos) => {
-            const dx = Math.abs(curr_pos - pos);
-            return count * dx;
+            return count * get_cost(curr_pos, pos);
         }).reduce((prev, curr) => curr + prev, 0);
     });
+
     return Math.min(...costs);
+
+}
+
+function part2(inputs: number[]) {
+    return findLowestCost(inputs, (pos, target) => {
+
+
+
+        return 0;
+    });
+}
+
+function part1(inputs: number[]) {
+    return findLowestCost(inputs, (pos, target) => Math.abs(pos - target))
 }
 
 function main() {
